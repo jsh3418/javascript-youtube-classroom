32c7ea7350841526e78206501838261a5b29afc0
jsh3418 <albert5428@gmail.com>
2022-09-17 13:13:49 +0900

    refactor: 영상 검색 엔터 keypress, 버튼 click 이벤트를 submit 이벤트로 수정

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,16 @@
 import { $ } from "./utils/DOM.js";
 import { handleSearchModalButton } from "./controller/handleSearchModalButton.js";
-import { handleSearchKeywordSubmit } from "./controller/handleSearchKeywordSubmit.js";
 import { handleSearchButton } from "./controller/handleSearchButton.js";
 import { searchResultInfiniteScroll } from "./controller/searchResultInfiniteScroll.js";
 
 const $searchModalButton = $("#search-modal-button");
-const $searchInputKeyword = $("#search-input-keyword");
-const $searchButton = $("#search-button");
+
+const $searchInput = $(".search-input");
 const $videoList = $(".video-list");
 
 const initApp = () => {
   $searchModalButton.addEventListener("click", handleSearchModalButton);
-  $searchInputKeyword.addEventListener("keypress", handleSearchKeywordSubmit);
-  $searchButton.addEventListener("click", handleSearchButton);
+  $searchInput.addEventListener("submit", handleSearchButton);
   $videoList.addEventListener("scroll", searchResultInfiniteScroll);
 };
 
