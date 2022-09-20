8c3db040b964bc547ef9b6cd9bd82c4ee125bad7
jsh3418 <albert5428@gmail.com>
2022-09-20 22:31:42 +0900

    feat: 저장 가능한 최대 동영상의 개수를 100개로 제한

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -1,3 +1,4 @@
 export const LOAD_VIDEO_COUNT = 10;
 export const BASE_URL = "https://www.googleapis.com/youtube/v3/search?";
 export const LOCALSTORAGE_KEY = "saveVideos";
+export const SAVE_VIDEOS_MAX_COUNT = 100;
--- src/js/controller/handleVideoSaveButton.js
+++ src/js/controller/handleVideoSaveButton.js
@@ -1,10 +1,10 @@
-import { LOCALSTORAGE_KEY } from "../constants/constants.js";
+import { LOCALSTORAGE_KEY, SAVE_VIDEOS_MAX_COUNT } from "../constants/constants.js";
 import { addSaveVideos, store } from "../model/model.js";
 import { setLocalStorage } from "../utils/utils.js";
 import { hideSaveButton } from "../view/saveVideos.js";
 
 export const handleVideoSaveButton = (event) => {
-  if (event.target.closest("button")) {
+  if (event.target.closest("button") && store.saveVideos.length <= SAVE_VIDEOS_MAX_COUNT) {
     const videoList = event.target.closest("li");
 
     addSaveVideos(videoList);
