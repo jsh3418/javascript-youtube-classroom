9a051cb114cdf34f0a173cd9dd876e99a13324ff
jsh3418 <albert5428@gmail.com>
2022-09-22 20:36:46 +0900

    feat: 최근 검색어 3개까지 띄우는 기능 구현

--- src/js/controller/handleVideoSaveButton.js
+++ src/js/controller/handleVideoSaveButton.js
@@ -4,7 +4,6 @@ import { setLocalStorage } from "../utils/utils.js";
 import { hideElement } from "../view/view.js";
 
 export const handleVideoSaveButton = (event) => {
-  console.log(store.saveVideos.length);
   if (event.target.closest("button") && store.saveVideos.length <= SAVE_VIDEOS_MAX_COUNT) {
     const videoList = event.target.closest("li");
 
--- src/js/view/recentSearchText.js
+++ src/js/view/recentSearchText.js
@@ -3,15 +3,15 @@ import { store } from "../model/model.js";
 
 const recentSearchTextTemplate = (text) => {
   return `
-  <span>${text}</span>
+    <span class="recent-search-text">${text}</span>
   `;
 };
 
 export const paintRecentSearchText = () => {
-  const $recentSearchText = $(".recent-search-text");
-  let template = "";
+  const $recentSearchTextWrapper = $(".recent-search-text-wrapper");
+  let template = "최근 검색어:";
   store.recentSearchText.forEach((text) => {
     template += recentSearchTextTemplate(text);
   });
-  $recentSearchText.innerHTML = template;
+  $recentSearchTextWrapper.innerHTML = template;
 };
