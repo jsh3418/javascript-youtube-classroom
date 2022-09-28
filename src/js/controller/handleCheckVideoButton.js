6035f03bceab858b210582880adb2f04a2690769
jsh3418 <albert5428@gmail.com>
2022-09-28 22:31:39 +0900

    fix: 체크버튼인지 확인하는 함수 호출이 되지 않은 부분 수정

--- src/js/controller/handleCheckVideoButton.js
+++ src/js/controller/handleCheckVideoButton.js
@@ -3,9 +3,10 @@ import { LOCALSTORAGE_KEY } from "../constants/constants.js";
 import { setLocalStorage } from "../utils/utils.js";
 
 export const handleCheckVideoButton = (event) => {
-  if (isCheckButton) {
+  const eventTarget = event.target;
+  if (isCheckButton(eventTarget)) {
     store.laterSeeVideos.forEach((video) => {
-      if (video.dataVideoId === event.target.closest("li").dataset.videoId) {
+      if (video.dataVideoId === eventTarget.closest("li").dataset.videoId) {
         video.isChecked = true;
       }
     });
@@ -13,6 +14,6 @@ export const handleCheckVideoButton = (event) => {
   setLocalStorage(LOCALSTORAGE_KEY, store.laterSeeVideos);
 };
 
-const isCheckButton = (event) => {
-  return event.target.closest("button") && event.target.closest("button").classList.contains("check-video-button");
+const isCheckButton = (eventTarget) => {
+  return eventTarget.closest("button") && eventTarget.closest("button").classList.contains("check-video-button");
 };
