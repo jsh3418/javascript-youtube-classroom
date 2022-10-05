263ce340e1b629a74513c68de9ef233417a37e4a
jsh3418 <albert5428@gmail.com>
2022-10-05 19:13:37 +0900

    fix: 볼 영상에서 체크버튼을 클릭하면 동작하지 않던 오류 수정

--- src/js/controller/handleCheckVideoButton.js
+++ src/js/controller/handleCheckVideoButton.js
@@ -4,26 +4,29 @@ import { setLocalStorage } from "../utils/utils.js";
 import { clearLaterSeeVideos, paintSaveVideos } from "../view/saveVideos.js";
 import { showSnackbar } from "../view/snackBar.js";
 
+let isUncheck;
 export const handleCheckVideoButton = (event) => {
   const eventTarget = event.target;
 
   if (!isCheckButton(eventTarget)) return;
 
-  checkStoreSaveVideo(eventTarget);
+  toggleCheckVideo(eventTarget);
   clearLaterSeeVideos();
   paintSaveVideos(store.currentPage);
   setLocalStorage(LOCALSTORAGE_KEY, store.saveVideos);
-  showSnackbar("본 영상으로 처리하였습니다.");
+
+  isUncheck ? showSnackbar("본 영상으로 처리하였습니다.") : showSnackbar("볼 영상으로 처리하였습니다.");
 };
 
 const isCheckButton = (eventTarget) => {
   return eventTarget.classList.contains("check-video-button");
 };
 
-const checkStoreSaveVideo = (eventTarget) => {
+const toggleCheckVideo = (eventTarget) => {
   store.saveVideos.forEach((video) => {
     if (video.dataVideoId === eventTarget.closest("li").dataset.videoId) {
-      video.isChecked = true;
+      video.isChecked = !video.isChecked;
+      isUncheck = video.isChecked;
     }
   });
 };
